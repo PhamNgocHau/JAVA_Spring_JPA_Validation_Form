0676688be3d9665330447d3a577b0cee23afc4a3
phamngochau1506@gmail.com <haupham156>
2019-10-24 14:47:41 +0700

    Spring_JPA_Validation_Form

---
+++ src/main/java/com/codegym/repository/UserRepositoryImpl.java
@@ -0,0 +1,29 @@
+package com.codegym.repository;
+
+import com.codegym.model.User;
+import org.springframework.ui.Model;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+
+@Transactional
+public class UserRepositoryImpl implements UserRepository {
+    @PersistenceContext
+    private EntityManager em;
+    @Override
+    public List<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public void save(User model) {
+        if (model.getId() != null){
+            em.merge(model);
+        } else {
+            em.persist(model);
+        }
+    }
+}
